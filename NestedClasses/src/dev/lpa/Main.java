5d65a4336f54c129d24889ec43e98ccc84f2a595
Kajee <147959051+kajee-engineering@users.noreply.github.com>
2025-06-25 06:53:44 +0900

    Add files via upload

--- NestedClasses/src/dev/lpa/Main.java
+++ NestedClasses/src/dev/lpa/Main.java
@@ -42,7 +42,55 @@ public class Main {
         for (StoreEmployee e : storeEmployees) {
             System.out.println(e);
         }
+
+        System.out.println("With Pig Latin Names");
+        addPigLatinName(storeEmployees);
+
     }
 
+    public static void addPigLatinName(List<? // ジェネリック・ワイルドカード は引数の型の上限を指定。StoreEmployeeまたはそのサブタイプを含むリストのみを受け付けることを意味する
+            extends StoreEmployee > list) {
 
+        String lastName = "Piggy";
+
+        // なぜクラスの中にクラスを作るのか外部に公開する必要のないクラスを作りたい時もある。データが密接につながっていて、隠蔽したい時もある。誰みも使ってほしくないし、拡張もしてほしくない。特定の目的のためだけに存在してほしい。
+        class DecoratedEmployee extends StoreEmployee
+        implements Comparable<DecoratedEmployee>{
+
+            private String pigLatinName;
+            private Employee originalInstance;
+
+            public DecoratedEmployee(String pigLationName, Employee originalInstance) {
+                this.pigLatinName = pigLationName + " " + lastName;
+                this.originalInstance = originalInstance;
+            }
+
+            @Override
+            public String toString() {
+                return originalInstance.getName() + " " + pigLatinName;
+            }
+
+            @Override
+            public int compareTo(DecoratedEmployee o) {
+                return pigLatinName.compareTo(o.pigLatinName);
+            }
+        }
+
+        List<DecoratedEmployee> newList = new ArrayList<>(list.size()); // クラスのListのサイズに揃える
+
+        for (var employee : list) {
+            String name = employee.getName();
+            String pigLatin = name.substring(1) + name.charAt(0) + "ay";
+            newList.add(new DecoratedEmployee(pigLatin, employee));
+        }
+
+        newList.sort(null); // nullを渡すとComparableクラスのcompareToメソッドが使われる
+        for (var dEmployee : newList) {
+            // インナークラスは外側の変数や引数をキャプチャできるが、それらは「実質final」でなければならない。
+            // キャプチャされた変数は、インナークラスのインスタンスごとにコピーされるのではなく、参照として保持される（ただし値型は値がコピーされるように見える）。
+            // Java 8以降は「実質final」でもOK。
+            // lastName = "Latin"; // 値を再代入しているのでコンパイルエラーになる
+        }
+
+    }
 }
